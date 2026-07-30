c682e945f73f1cc26e58791ec3a7317ee83b88d4
TrishaJambaro <256789705+hystrawr@users.noreply.github.com>
2026-07-30 21:42:14 +0800

    asdsa

--- bumpcarts/src/App.tsx
+++ bumpcarts/src/App.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import Navbar from './components/Navbar'
 
 export default function App() {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
-      <h1 className="text-4xl font-bold text-white">Grid of products</h1>
-      <p className="text-lg text-gray-300">blablabla</p>
+  return ( 
+    <div className="min-h-screen bg-black text-white">
+      <Navbar />
+    
     </div>
-  );
+  )
 }
